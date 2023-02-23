ada7ea09a76da1f219fbdb7d063faf8b3993720c
muriki <mgy20@inf.ufpr.br>
2023-02-23 09:49:06 -0300

    Added packages

--- config.sh
+++ config.sh
@@ -2,11 +2,12 @@
 sudo pacman -Syyu
 
 #instalação de sistema
-sudo pacman -S networkmanager dhcpcd speedtest-cli i3 i3status i3lock vim neovim alacritty pulseaudio
+sudo pacman -S networkmanager dhcpcd speedtest-cli i3 i3status i3lock vim neovim alacritty pulseaudio picom
 
 #instalacao de programas basicos
 sudo pacman -S git valgrind gdb xclip unzip evince feh htop \
   ttf-nerd-fonts-symbols-1000-em noto-fonts-cjk noto-fonts-emoji noto-fonts \
+  bash-completion
 
 #instalação de programas adicionais
 sudo pacman -S discord telegram-desktop spotify-launcher 
