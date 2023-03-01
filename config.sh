a701655d8d6979d28597c2d858c5c308569e939b
Muriki G. Yamanaka <64929739+MurikiGY@users.noreply.github.com>
2023-02-28 22:33:56 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -12,6 +12,16 @@ sudo pacman -S git valgrind gdb xclip unzip evince feh htop \
 #instalação de programas adicionais
 sudo pacman -S discord telegram-desktop spotify-launcher 
 
+#Japanese Input
+sudo pacman -S fcitx5 fcitx5-configtool fcitx5-mocz fcitx5-qt fcitx5-gtk
+#Add in /etc/environment
+GTK_IM_MODULE=fcitx
+QT_IM_MODULE=fcitx
+XMODIFIERS=@im=fcitx
+#Fcitx autostart
+cp /usr/share/applications/org.fcitx.Fcitx5.desktop ~/.config/autostart/
+#Config the fcitx5-configtool after!!
+
 #instalacao do yay
 git clone https://aur.archlinux.org/yay.git
 cd yay
