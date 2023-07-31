e4e04de961a40f10a530f419845ad86e4f2201d5
Muriki G. Yamanaka <64929739+MurikiGY@users.noreply.github.com>
2023-07-31 16:08:03 -0300

    Update tree.lua

--- config/nvim/after/plugin/tree.lua
+++ config/nvim/after/plugin/tree.lua
@@ -2,12 +2,14 @@
 --vim.g.loaded_netrw = 1
 --vim.g.loaded_netrwPlugin = 1
 
-local function open_nvim_tree()
-  require("nvim-tree.api").tree.open()
-end
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
-
 require("nvim-tree").setup()
 
 vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
 vim.keymap.set("n", "<leader>b", vim.cmd.bn)
+
+--local function open_nvim_tree()
+--  require("nvim-tree.api").tree.open()
+--end
+--vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+
