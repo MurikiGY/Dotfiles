7759ff9e7b19e56dc04a9a261986d17f8a1fc943
Muriki G. Yamanaka <64929739+MurikiGY@users.noreply.github.com>
2024-03-25 22:30:24 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,7 +3,8 @@ sudo pacman -Syyu
 
 #instalação de sistema
 sudo pacman -S base-devel dhcpcd iwd xorg xorg-xinit i3 i3status i3lock i3block \
-  vim neovim alacritty pulseaudio alsa-utils arandr autorandr picom
+pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack alsa-utils playerctl \
+vim neovim alacritty arandr autorandr picom
 
 #instalacao de programas basicos
 sudo pacman -S git xclip unzip evince feh htop speedtest-cli tmux \
